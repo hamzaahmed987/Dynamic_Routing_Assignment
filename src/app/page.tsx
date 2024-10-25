6ddefd4d6745b799186d20e8b455d810ddaabdb5
Hamza Ahmed <159677434+hamzaahmed987@users.noreply.github.com>
2024-10-25 20:34:14 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import "./globals.css"
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Countries List</h2>
+      <h3>Click on any country button to show its data: </h3>
+      <ul className="d-flex">
+            <li className="cust_button"><Link className="" href="./country/pakistan">Pakistan</Link></li>
+            <li className="cust_button"><Link className="" href="./country/india">India</Link></li>
+            <li className="cust_button"><Link className="" href="./country/afghanistan">Afganistan</Link></li>
+            <li className="cust_button"><Link className="" href="./country/china">China</Link></li>
+            <li className="cust_button"><Link className="" href="./country/japan">Japan</Link></li>
+        </ul>
+    </div>
+  );
+}
